09266d2274d7f426872d54e1deedd715a2a4fe5d
Gabriel <gerson.santos@sgnsistemas.com.br>
2025-03-27 15:23:58 -0300

    feat: adiciona CRUD tabela estoque

---
+++ src/domain/repository/estoque.repository.ts
@@ -0,0 +1,10 @@
+import { IEstoque } from "../entity/estoque.entity";
+
+export interface EstoqueRepository {
+  selectAll(): Promise<IEstoque[]>;
+  selectById(id: number): Promise<IEstoque | null>;
+  selectByDescricao(descricao: string | null): Promise<IEstoque | null>;
+  insert(input: IEstoque): Promise<IEstoque>;
+  update(id: number, input: IEstoque): Promise<IEstoque>;
+  delete(id: number): Promise<void>;
+}
---
+++ src/infrastructure/database/repository/estoque.repository.ts
@@ -0,0 +1,114 @@
+import AppError from "../../../application/errors/appError";
+import { getErrorMessage } from "../../../constraints/sql.errors.code";
+import { IEstoque } from "../../../domain/entity/estoque.entity";
+import { EstoqueRepository } from "../../../domain/repository/estoque.repository";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default class EstoqueDatabaseRepository implements EstoqueRepository {
+  async selectAll(): Promise<IEstoque[]> {
+    try {
+      const index = await prisma.estoque.findMany({
+        select: {
+          id: true,
+          quantidade: true,
+          descricao: true,
+          data_atualizacao: true,
+        },
+      });
+      return index;
+    } catch (error: any) {
+      getErrorMessage(error);
+      throw new AppError("Erro desconhecido", error);
+    }
+  }
+  async selectById(id: number): Promise<IEstoque | null> {
+    try {
+      const show = await prisma.estoque.findUnique({
+        select: {
+          id: true,
+          quantidade: true,
+          descricao: true,
+          data_atualizacao: true,
+        },
+        where: { id },
+      });
+
+      return show;
+    } catch (error: any) {
+      getErrorMessage(error);
+      throw new AppError("Erro desconhecido", error);
+    }
+  }
+  async selectByDescricao(descricao: string): Promise<IEstoque | null> {
+    try {
+      const show = await prisma.estoque.findFirst({
+        select: {
+          id: true,
+          descricao: true,
+          quantidade: true,
+          data_atualizacao: true,
+        },
+        where: {
+          descricao,
+        },
+      });
+      return show;
+    } catch (error: any) {
+      getErrorMessage(error);
+      throw new AppError("Erro desconhecido", error);
+    }
+  }
+  async insert(input: IEstoque): Promise<IEstoque> {
+    try {
+      const { descricao, quantidade, data_atualizacao } = input;
+
+      const show = prisma.estoque.create({
+        data: {
+          descricao,
+          quantidade,
+          data_atualizacao: data_atualizacao
+            ? new Date(data_atualizacao)
+            : new Date(),
+        },
+      });
+      return show;
+    } catch (error: any) {
+      getErrorMessage(error);
+      throw new AppError("Erro desconhecido", error);
+    }
+  }
+  async update(id: number, input: IEstoque): Promise<IEstoque> {
+    try {
+      const { descricao, quantidade, data_atualizacao } = input;
+      const show = await prisma.estoque.update({
+        data: {
+          descricao,
+          quantidade,
+          data_atualizacao: data_atualizacao
+            ? new Date(data_atualizacao)
+            : new Date(),
+        },
+        where: {
+          id,
+        },
+      });
+      return show;
+    } catch (error: any) {
+      getErrorMessage(error);
+      throw new AppError("Erro desconhecido", error);
+    }
+  }
+  async delete(id: number): Promise<void> {
+    try {
+      await prisma.estoque.delete({
+        where: { id },
+      });
+      return;
+    } catch (error: any) {
+      getErrorMessage(error);
+      throw new AppError("Erro desconhecido", error);
+    }
+  }
+}
