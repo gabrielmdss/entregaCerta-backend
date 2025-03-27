bb76d68e577ece969199a3f08fb2a7836952aa4e
Gabriel <gerson.santos@sgnsistemas.com.br>
2025-03-27 09:51:49 -0300

    feat: substitui querys Rows por prisma client

--- src/domain/repository/assistido.repository.ts
+++ src/domain/repository/assistido.repository.ts
@@ -2,11 +2,11 @@ import { IAssistido } from "../entity/assistido.entity";
 
 export interface AssistidoRepository {
     selectAll(): Promise<IAssistido[]>;
-    selectById(id: number): Promise<IAssistido>;
+    selectById(id: number): Promise<IAssistido | null>;
     insert(input: IAssistido): Promise<IAssistido>;
     update(id: number, input: IAssistido): Promise<IAssistido>;
     delete(id: number): Promise<void>;
     //==================================================>
-    selectByDocumento(documento: string): Promise<IAssistido>;
+    selectByDocumento(documento: string): Promise<IAssistido | null>;
     countAllAssistidos(): Promise<number>;
 }
